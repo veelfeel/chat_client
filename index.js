fda21dc8a461372821a2d4ec82ee98ecaa152cf8
viktorfilmanovich <film.viktor@yandex.ru>
2023-07-28 15:51:36 +0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sender_id = (Date.now() / 1000) | 0;
-console.log(1);
+
 const ws = new WebSocket("wss://chat-cpwa.onrender.com");
 ws.addEventListener("open", () => {
   console.log("we are connected");
@@ -8,7 +8,9 @@ ws.addEventListener("open", () => {
 ws.addEventListener("message", (e) => {
   const data = JSON.parse(e.data);
   if (sender_id !== data.sender_id) {
-    const messageHtml = `<div>user ${data.sender_id}: ${data.message}</div>`;
+    const messageHtml = `<div class="message message-recipient">
+                          <div class="sent-message">${data.message}</div>
+                        </div>`;
 
     document
       .getElementById("chatBody")
@@ -20,17 +22,43 @@ document
   .getElementById("sendButton")
   .addEventListener("click", sendMessageToTheServer);
 
+document.getElementById("textMessage").addEventListener("keydown", (e) => {
+  if (
+    e.code == "Enter" &&
+    document.getElementById("textMessage").value !== ""
+  ) {
+    sendMessageToTheServer();
+  }
+});
+
 function sendMessageToTheServer() {
-  const message = document.getElementById("textMessage").value;
-  const messageHtml = `<div>user ${sender_id}: ${message}</div>`;
+  if (document.getElementById("textMessage").value !== "") {
+    const message = document.getElementById("textMessage").value;
+    const messageHtml = `<div class="message message-sender">
+                          <div class="sent-message">${message}</div>
+                        </div>`;
 
-  document
-    .getElementById("chatBody")
-    .insertAdjacentHTML("beforeend", messageHtml);
+    document
+      .getElementById("chatBody")
+      .insertAdjacentHTML("beforeend", messageHtml);
 
-  const jsonData = { sender_id: sender_id, message: message };
+    const jsonData = { sender_id: sender_id, message: message };
 
-  ws.send(JSON.stringify(jsonData));
+    ws.send(JSON.stringify(jsonData));
 
-  document.getElementById("textMessage").value = "";
+    document.getElementById("textMessage").value = "";
+    hideShowSendButton();
+  }
+}
+
+document
+  .getElementById("textMessage")
+  .addEventListener("input", hideShowSendButton);
+
+function hideShowSendButton() {
+  document.getElementById("textMessage").value !== ""
+    ? document
+        .getElementById("sendButton")
+        .classList.remove("visibility-hidden")
+    : document.getElementById("sendButton").classList.add("visibility-hidden");
 }
